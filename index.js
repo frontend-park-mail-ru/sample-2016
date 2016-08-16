bd732bf0814a09cbf251b4cabd1ff21d43d4c707
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2016-08-16 22:29:59 +0300

    specify node version

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
     res.end('Hello, world!');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App started on port ${process.env.PORT || 3000}`);
+});
