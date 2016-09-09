b359bd876beb3bf774b8fc42f2461432190434e3
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2016-09-09 20:29:15 +0300

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/frontend-park-mail-ru/sample.svg?branch=master)](https://travis-ci.org/frontend-park-mail-ru/sample)
--- test.js
+++ test.js
@@ -2,4 +2,3 @@ let assert = require('assert');
 let hello = require('./public/main').hello
 
 assert.equal(hello('Test'), 'Привет, Test');
-assert.equal(hello('Test'), 'Пока, Test');
