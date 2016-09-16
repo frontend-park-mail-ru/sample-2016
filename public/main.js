996547501b996e442763645b43f0126340677a77
Artem Mezin <iketari@gmail.com>
2016-09-16 18:49:25 +0300

    add linter. create first module

--- public/main.js
+++ public/main.js
@@ -1,78 +1,86 @@
-'use strict';
+(function () {
+	'use strict';
 
-let userData = {};
+	let userData = {};
 
-function filter (str, rules = ['КЕК']) {
-	return `//TODO: реализовать filter`;
-}
+	function filter (str, rules = ['КЕК']) {
 
-function onLogin (form, block) {
-	userData = {
-		user: form.elements['user'].value,
-		email: form.elements['email'].value
-	};
-
-	 jsLogin.hidden = true;
-	 jsChat.hidden = false;
-
-	 if (userData.user) {
-		 userData.user = filter(userData.user);
-		 jsTitle.innerHTML = jsTitle.innerHTML.replace('%username%', userData.user);
-	 }
-
-	 subscribe();
-}
-
-function createMessage (opts, isMy = false) {
-	let message = document.createElement('div');
-	let email = document.createElement('div');
-
-	message.classList.add('chat__message');
-	email.classList.add('chat__email');
-
-	if (isMy) {
-		message.classList.add('chat__message_my');
-	} else {
-		message.style.backgroundColor = `#${technolibs.colorHash(opts.email || '')}`;
+		return `//TODO: реализовать filter`;
 	}
-	message.innerHTML = opts.message;
-	email.innerHTML = opts.email;
-	message.appendChild(email);
+
+	function onLogin (form, block) {
+		userData = {
+			user: form.elements['user'].value,
+			email: form.elements['email'].value
+		};
+
+		 jsLogin.hidden = true;
+		 jsChat.hidden = false;
+
+		 if (userData.user) {
+			 userData.user = filter(userData.user);
+			 jsTitle.innerHTML = jsTitle.innerHTML
+			 	.replace('%username%', userData.user);
+		 }
+
+		 subscribe();
+	}
+
+	function createMessage (opts, isMy = false) {
+		let message = document.createElement('div');
+		let email = document.createElement('div');
+
+		message.classList.add('chat__message');
+		email.classList.add('chat__email');
+
+		if (isMy) {
+			message.classList.add('chat__message_my');
+		} else {
+			message.style.backgroundColor = `#${technolibs.colorHash(opts.email || '')}`;
+		}
+		message.innerHTML = opts.message;
+		email.innerHTML = opts.email;
+		message.appendChild(email);
 
 
-	return message;
-}
+		return message;
+	}
 
-function onChat (form) {
-	let data = {
-		message: form.elements['message'].value,
-		email: userData.email
-	};
+	function onChat (form) {
+		let data = {
+			message: form.elements['message'].value,
+			email: userData.email
+		};
 
-	let result = technolibs.request('/api/messages', data);
-	form.reset();
-}
+		let result = technolibs.request('/api/messages', data);
+		form.reset();
+	}
 
-function renderChat (items) {
-	jsMessages.innerHTML = '';
-	items.forEach(item => {
-		let message = createMessage(item, item.email === userData.email);
-		jsMessages.appendChild(message);
-	});
-	jsMessages.scrollTop = jsMessages.scrollHeight;
-}
+	function renderChat (items) {
+		jsMessages.innerHTML = '';
+		items.forEach(item => {
+			let message = createMessage(item, item.email === userData.email);
+			jsMessages.appendChild(message);
+		});
+		jsMessages.scrollTop = jsMessages.scrollHeight;
+	}
 
-function subscribe () {
-	technolibs.onMessage(data => {
-		renderChat(Object.keys(data).map(key => data[key]));
-	});
-}
+	function subscribe () {
+		technolibs.onMessage(data => {
+			renderChat(Object.keys(data).map(key => data[key]));
+		});
+	}
 
-function hello(text) {
-	return 'Привет, ' + text;
-}
+	function hello(text) {
+		return 'Привет, ' + text;
+	}
 
-if (typeof exports === 'object') {
-	exports.hello = hello;
-	exports.filter = filter;
-}
+	if (typeof exports === 'object') { // for NodeJS
+		exports.hello = hello;
+		exports.filter = filter;
+	} else {
+		window.onLogin = onLogin;
+		window.onChat = onChat;
+	}
+
+})();
