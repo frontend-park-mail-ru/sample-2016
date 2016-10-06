311fd13c04747dedb9f2bd68c42500ed7839fb9d
Artem Mezin <iketari@gmail.com>
2016-10-06 22:12:53 +0300

    karma install

---
+++ test/modules/Router.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe("A test suite", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should fail', function() { expect(true).to.be.false; });
+});
