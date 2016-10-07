d8044641f20b37e15e3e90d1a982849e1723d19f
Artem Mezin <iketari@gmail.com>
2016-10-07 13:18:26 +0300

    remove unusable test

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-let assert = require('assert');
-let hello = require('./public/main').hello;
-let filter = require('./public/main').filter;
-
-
-
-assert.equal(hello('Test'), 'Привет, Test');
-//TODO: Кейсы для функции filter
-// assert.equal(filter('КЕК'), '***');
+console.warn('TODO: swich to framework for testing!');
