a888a95570d4742f97647eea1232883eaba5f459
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2016-10-07 16:30:13 +0300

    Дописаны тесты на View

--- test/modules/view.spec.js
+++ test/modules/view.spec.js
@@ -1,4 +1,31 @@
 (function () {
 	'use strict';
 
+
+	describe('Класс View', function () {
+		beforeEach(function () {
+			this.view = new View({createElement: true});
+		});
+
+		it('Если при создании view задана опция createElement, то создастся элемент div', function () {
+			expect(this.view._el).toBeDefined();
+			expect(this.view._el.tagName.toLowerCase()).toBe('div');
+		});
+
+		describe('View.fn.resume', function () {
+			it('по умолчанию устанавливает элементу атрибут hidden в false', function () {
+				this.view.resume();
+				expect(this.view._el.hidden).toBe(false);
+			});
+		});
+
+
+		describe('View.fn.pause', function () {
+			it('по умолчанию устанавливает элементу атрибут hidden в true', function () {
+				this.view.pause();
+				expect(this.view._el.hidden).toBe(true);
+			});
+		});
+
+	})
 })();
