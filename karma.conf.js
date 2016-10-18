d28aa76f53c8b5869217f04e55349fbbefcd98f9
avtyul <avtyul@gmail.com>
2016-10-18 23:28:52 +0300

    plugins

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,11 @@ module.exports = function (config) {
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-
+		plugins: [
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			'karma-coverage'
+		],
 		browsers: ['Chrome'],
 		customLaunchers: {
 			Chrome_travis_ci: {
