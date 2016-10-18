b5a0533320c971d10ef1de12d5eb895a974153bd
avtyul <avtyul@gmail.com>
2016-10-18 23:39:26 +0300

    lint dirty hack

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,17 @@ module.exports = {
     "extends": "eslint:recommended",
     "plugins": [
         "import"
-    ]
+    ],
+    "env": {
+        "es6": true,
+        "node": true,
+        "browser": true
+    },
+    "rules": {
+        "no-unused-vars": [1, { "vars": "all", "args": "none" }],
+        "no-extra-semi": 1,
+        "no-undef": 1,
+        "no-console": 1,
+        "no-unused-vars": 1
+    }
 };
